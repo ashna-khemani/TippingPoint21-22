931b23e9c58549f83b320b2128cee5088b18c336
ashna-khemani <ashna.khemani@gmail.com>
2022-01-02 16:35:54 -0800

    New project for pneumatics testing
    New project to figure out how pneumatics work. To avoid messing with/getting confused by other stuff in main TippingPoint project folder.

---
+++ PneumaticsTest/src/main.cpp
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       ashnakhemani                                              */
+/*    Created:      Sun Jan 02 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  
+}
