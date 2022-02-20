1d68cb9d8e65bad8cac051883eda668e18f110ea
ashna-khemani <ashna.khemani@gmail.com>
2022-02-20 11:38:24 -0800

    Code used in Sig (2/19)
    Uhh whatever we had at the end of the day on sig. Skills auton worked pretty well on 2/18. Not on the actual day of sig because drive train decided to die :)

---
+++ TippingPt_v1_matchTaskSig/include/auton-functions.h
@@ -0,0 +1,13 @@
+void NGoalInFront();    // Right
+void NGoalInFrontL();   // Longer (for left side)
+
+// Right AWP
+void NGoalAndRAWP();
+
+// First Skills auton program
+void Skills_1();
+void Skills_Plat();
+void Skills_Plat2();
+void Skills_Push();
+
+void Middle_goal();
